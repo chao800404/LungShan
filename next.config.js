ff9b79e34e5fac24312e31452f36ebd6d75eb92c
chao <a0921342997@gmail.com>
2023-04-23 13:06:31 +0800

    Update (Nextjs config file)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/wp',
+        destination: process.env.LUNG_SHAN_WORDPRESS,
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
